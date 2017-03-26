4b36c143a6c1f45ce2900362238b49a1d1e0b8ea
Chen Lu <chenlucircle@gmail.com>
2017-03-26 19:23:01 -0400

    cors

--- server/ServerRouter.js
+++ server/ServerRouter.js
@@ -8,14 +8,14 @@ const app = express();
 var mongoapi = require('./mongo.api.js');
 
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT||2000, function() {
   console.log('listening on 2000')
 
    //console.log(JSON.stringify(mongoaip.getAllPosts()));
 });
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+ // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Content-Type', 'application/json');
   next();
